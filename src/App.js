6f583d2f7ae13b1beed9799bf71ed79b63405c31
Archer <arseniy.tarapon@gmail.com>
2019-01-31 18:52:06 +0600

    Добавил функцию для создания чисел и подключил ее

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
-import {Numbers} from "./components";
+import {Number, Header} from "./components";
+
+function createArray() {
+    let nums = [], numsLen = 5, maxNum = 36, minNum = 5;
+    while (nums.length < numsLen) {
+        let num = Math.round(Math.random() * (maxNum - minNum)) + minNum;
+        if (nums.indexOf(num) === -1) {
+            nums.push(num);
+        }
+    }
+    nums.sort(function (a, b) {
+        return a - b
+    });
+    return nums;
+}
+
 
 class App extends Component {
-  render() {
-    return (
-        <div className='App'>
-            <Numbers/>
-        </div>
-    );
-  }
+
+    state = {
+        numbers: []
+    };
+
+    newNumbers = () => {
+        let numbers = createArray();
+        this.setState({numbers})
+    };
+
+    render() {
+        return (
+            <div className='App'>
+                <Header/>
+                <div className='container'>
+                    <button onClick={this.newNumbers}>получить числа</button>
+                    <div>
+                        {this.state.numbers.map(number => {
+                            return <Number number={number}/>
+                        })}</div>
+                </div>
+            </div>
+        );
+    };
 }
 
 export default App;
