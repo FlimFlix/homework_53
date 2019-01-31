dab0b091505b42178c029bfaa0fd0dcd7059ab47
Archer <arseniy.tarapon@gmail.com>
2019-01-31 14:59:15 +0600

    Создал файлы для компонентов и стилей и подключил их

---
+++ src/components.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css'
+
+
+function Numbers() {
+    return (
+        <div className='container'>
+            <h1>Numbers</h1>
+        </div>
+    )
+}
+
+export {Numbers}
